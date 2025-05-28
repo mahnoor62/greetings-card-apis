d8f988c56882263850fb3a6e82712fb253f8df60
mahnoor62 <148554953+mahnoor62@users.noreply.github.com>
2025-05-28 16:57:28 +0500

    Update bank.js

--- models/bank.js
+++ models/bank.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-
-const bankSchema = new mongoose.Schema({
-    name: {type: String},
-    account_no: {type: String},
-    iban: {type: String},
-    description: {type: String},
-});
-
-module.exports = mongoose.model('bank', bankSchema);
+// const mongoose = require('mongoose');
+//
+// const bankSchema = new mongoose.Schema({
+//     name: {type: String},
+//     account_no: {type: String},
+//     iban: {type: String},
+//     description: {type: String},
+// });
+//
+// module.exports = mongoose.model('bank', bankSchema);
